6db6415f42d02a53b14af6329f489bc746a48754
JackDevine <devja964@student.otago.ac.nz>
2016-07-26 15:46:15 +1200

    Ignore simulateSytem.jl

--- src/simulateSystem.jl
+++ src/simulateSystem.jl
@@ -2,7 +2,7 @@
 include("finiteDifferences.jl")
 using PyPlot
 
-v_0 = 0.8 # Amplitude of the oscillations in the potential
+v_0 = 0.08 # Amplitude of the oscillations in the potential
 ff = 3.0 # Forcing for the potential
 LL = 1.0 # Length of one period
 T0 = 3.0 # Temperature at the ends (i.e. bath temperature)
@@ -11,9 +11,9 @@ alpha = 0.0004
 beta = 0.15
 
 n_periods = 4 # The number of periods to stretch out for
-n_points = 2000 # The number of points on the x_axis
+n_points = 5000 # The number of points on the x_axis
 n_steps = 300 # The number of time steps to simulate for
-dt = 0.00005 # Size of the time step (keep this much smaller than the grid
+dt = 0.0005 # Size of the time step (keep this much smaller than the grid
              # spacing)
 
 x_axis = linspace(-LL*n_periods, LL*n_periods, n_points)
@@ -32,9 +32,9 @@ dis_temp0, dis_temp_end = temp_fun(x_axis[1] - dx), temp_fun(x_axis[end] + dx)
 dis_temp_tup = (dis_temp0, dis_temp, dis_temp_end)
 
 sigma  = 0.1
-# P_0 = Float64[(1/(sigma*sqrt(2pi)))*exp(-((x-0.0)^2)/(2sigma^2))
-#               for x in x_axis]
-P_0 = ones(x_axis)
+P_0 = Float64[(1/(sigma*sqrt(2pi)))*exp(-((x-1.0)^2)/(2sigma^2))
+              for x in x_axis]
+# P_0 = ones(x_axis)
 P_0 /= discrete_quad(P_0, x_axis[1], x_axis[end])
 temp = Array(Float64, n_points)
 P = Array(Float64, n_points, n_steps)
@@ -66,5 +66,6 @@ T[:, 1] = dis_temp
     end
 end
 
+n = 1
 time_step = n_steps
 plot(x_axis, 0.1dis_V, x_axis, P[:, time_step], x_axis, T[:, time_step])
