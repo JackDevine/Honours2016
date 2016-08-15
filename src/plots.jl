1fdd71e9be16a1570dfb71b99ae2ae710b3747bc
JackDevine <devja964@student.otago.ac.nz>
2016-08-15 13:33:00 +1200

    Revert "Functions can accept symbols to indicate the boundary conditions"
    This reverts commit 19a38b6bd8ce9360eef3c30bcba71485baf45b78.

--- src/plots.jl
+++ src/plots.jl
@@ -7,7 +7,7 @@ ff = 3.0 # Forcing for the potential
 LL = 1.0 # Length of one period
 T0 = 15.0 # Temperature at the ends (i.e. bath temperature)
 
-alpha = -0.000392
+alpha = -0.002
 betad = 0.005
 
 nPeriods = 6 # The number of periods to stretch out for
@@ -16,15 +16,14 @@ dt = 2e-5 # Size of the time step (keep this much smaller than the grid
              # spacing)
 
 # xAxis = linspace(-LL*nPeriods, LL*nPeriods, nPoints)
-xAxis = linspace(-2LL, 2LL, nPoints)
+xAxis = linspace(0LL, 5LL, nPoints)
 dx = (xAxis[end] - xAxis[1])/nPoints # Grid spacing
 
 # V(x, time) = ff*x^2 + 6ff # Potential
 sigma = 0.1
 bump = 9
 # V(x) = 0.2x^3 + 3.2ff*exp(-((x-bump)^2)/(2sigma^2))
-# V(x) = 2 - 18.7333x + 28.7222x^2 - 14.3889x^3 + 2.44444x^4 - 0.0444444x^5
-V(x) = 10*(1.9abs(x) - 1.1)^2
+V(x) = 28. - 56.35x + 37.1333x^2 - 9.65x^3 + 0.866667x^4
 potential = Float64[V(x) for x in xAxis]
 potential0, potentialEnd = V(xAxis[1] - dx), V(xAxis[end] + dx)
 potentialTup = (potential0, potential, potentialEnd)
@@ -34,7 +33,7 @@ temperatureFun(x) = T0
 temperature = Float64[temperatureFun(x) for x in xAxis]
 
 sigma  = 0.05
-P0 = Float64[(1/(sigma*sqrt(2pi)))*exp(-((x-0.6)^2)/(2sigma^2))
+P0 = Float64[(1/(sigma*sqrt(2pi)))*exp(-((x-4)^2)/(2sigma^2))
               for x in xAxis]
 # P0 = ones(xAxis)
 P0 /= discrete_quad(P0, xAxis[1], xAxis[end])
@@ -50,7 +49,7 @@ density, temp = evolve_system(density, temperature, evolve_time, dt, potentialTu
 density2 = evolveP(P0, evolve_time, dt, potentialTup, ones(xAxis)*T0, xAxis)
 end
 
-plot(xAxis, 0.05potential, xAxis, 5density, xAxis, 5density2, xAxis,
+plot(xAxis, 0.5potential, xAxis, 5density, xAxis, 5density2, xAxis,
             temp, xAxis, ones(xAxis)*T0)
 legend(["Potential", "Coupled probability density",
         "Uncoupled probability density", "Temperature", "Initial temperature"], loc=2)
--- src/runHopping.jl
+++ src/runHopping.jl
@@ -1,13 +1,14 @@
 # @everywhere include("simulateSteadyState.jl")
 # @everywhere using Hopping
+using PyPlot
 close("all")
 
 @everywhere begin
-nPoints = 500
-alphaMin = -0.0003
-alphaMax = -0.00029
+nPoints = 50
+alphaMin = -0.002
+alphaMax = -0.0001
 betaMin = 0.000001
-betaMax = 1.0
+betaMax = 0.001
 betaVec = linspace(betaMin, betaMax, nPoints)
 alphaVec = linspace(alphaMin, alphaMax, nPoints)
 alphaBeta = [[alpha, beta] for alpha in alphaVec, beta in betaVec]
@@ -24,25 +25,20 @@ end # @everywhere
 println("pmap:")
 @time c = pmap(z -> hopping_time(z[1], z[2]), alphaBeta[:])
 c = reshape(c, nPoints, nPoints)
-c = convert(Array{Float64, 2}, c)
+
+# println("Parallel loop")
+# d = SharedArray(Float64, nPoints, nPoints)
+# @time @sync @parallel for i in 1:nPoints
+#     for j in 1:nPoints
+#         d[i, j] = hopping_time(alphaVec[i], betaVec[j])
+#     end
+# end
+
 
 matshow(c)
-title("Hopping time for a bistable potential")
 xlabel(L"\beta")
 ylabel(L"\alpha")
 figure()
 surf(betaVec, alphaVec, c)
 xlabel(L"\beta")
 ylabel(L"\alpha")
-figure()
-surf(log(c))
-figure()
-matshow(log(c), extent=[betaVec[1], betaVec[end], 1e5*alphaVec[1],
- 1e5*alphaVec[end]])
-colorbar()
-title("Hopping time for a bistable potential (log scale)")
-xlabel(L"\beta")
-ylabel(L"\alpha \times 10^5")
-# @time c = pmap(alpha -> hopping_time(alpha, 0.05), alphaVec)
-# c = convert(Array{Float64, 1}, c)
-# plot(alphaVec, c)
