32a92a877f55841e9a30c2e2d058afaa054f6f8b
JackDevine <devja964@student.otago.ac.nz>
2016-08-15 13:39:46 +1200

    Use betad to avoid conflict with beta function

--- src/simulateSteadyState.jl
+++ src/simulateSteadyState.jl
@@ -1,5 +1,3 @@
-module Hopping
-export hopping_time, potentialTup, bump, P0, temperature, alpha, betad, xAxis
 include("finiteDifferences.jl")
 using PyPlot
 close("all")
@@ -7,14 +5,14 @@ close("all")
 v_0 = 0.08 # Amplitude of the oscillations in the potential
 ff = 3.0 # Forcing for the potential
 LL = 1.0 # Length of one period
-T0 = 15.0 # Temperature at the ends (i.e. bath temperature)
+T0 = 7.0 # Temperature at the ends (i.e. bath temperature)
 
 alpha = 0.0004
-betad = 0.1
+beta = 0.1
 
 nPeriods = 6 # The number of periods to stretch out for
 nPoints = 600 # The number of points on the xAxis
-dt = 2e-5 # Size of the time step (keep this much smaller than the grid
+dt = 1e-5 # Size of the time step (keep this much smaller than the grid
              # spacing)
 
 # xAxis = linspace(-LL*nPeriods, LL*nPeriods, nPoints)
@@ -24,7 +22,7 @@ dx = (xAxis[end] - xAxis[1])/nPoints # Grid spacing
 # V(x, time) = ff*x^2 + 6ff # Potential
 sigma = 0.1
 bump = 9
-V(x) = 0.25x^3 + 3.2ff*exp(-((x-bump)^2)/(2sigma^2))
+V(x) = 0.3x^3 + 5.0ff*exp(-((x-bump)^2)/(2sigma^2))
 potential = Float64[V(x) for x in xAxis]
 potential0, potentialEnd = V(xAxis[1] - dx), V(xAxis[end] + dx)
 potentialTup = (potential0, potential, potentialEnd)
@@ -47,7 +45,7 @@ boltzmann_density = boltzmann_density/discrete_quad(boltzmann_density,
 # evolve_time = 1000dt
 # @time begin
 # density, temp = evolve_system(density, temperature, evolve_time, dt, potentialTup,
-#                 alpha, betad, energy, xAxis)
+#                 alpha, beta, energy, xAxis)
 # density2 = evolveP(P0, evolve_time, dt, potentialTup, ones(xAxis)*T0, xAxis)
 # end
 #
@@ -58,7 +56,7 @@ boltzmann_density = boltzmann_density/discrete_quad(boltzmann_density,
 """
     hopping_time(potentialTup::Tuple{Number, AbstractArray, Number},
                 bump::Number, density::AbstractArray,
-                temperature::AbstractArray, alpha::Number, betad::Number,
+                temperature::AbstractArray, alpha::Number, beta::Number,
                 xAxis::AbstractArray; dt=1e-4, tol=0.1)
 Given an initial state of the system and a potential with a bump in it,
 calculate how long it takes for the probability distribution to get over
@@ -71,14 +69,14 @@ potential.
 * `temperature::AbstractArray`: The initial temperature of the system.
 * `alpha::Number`: Dimensionless parameter that controls the coupling of the
 system.
-* `betad::Number`: Dimensionless parameter that determines how fast the
+* `beta::Number`: Dimensionless parameter that determines how fast the
 temperature gradients diffuse.
 * `xAxis::AbstractArray`: The axis that we are working on.
 * `dt=1e-4`: Size of one time step for simulation.
 """
 function hopping_time(potentialTup::Tuple{Number, AbstractArray, Number},
                 bump::Number, density::AbstractArray,
-                temperature::AbstractArray, alpha::Number, betad::Number,
+                temperature::AbstractArray, alpha::Number, beta::Number,
                 xAxis::AbstractArray; dt=1e-4)
     bump_ind = indmin(abs(xAxis - bump))
     iters = 0
@@ -87,17 +85,38 @@ function hopping_time(potentialTup::Tuple{Number, AbstractArray, Number},
     while discrete_quad(density.*xAxis, xAxis[1], xAxis[end]) > bump
         density = stepP(density, dt, potentialTup, temperature, xAxis)
         temperature = stepT(temperature, dt, density, potentialTup, alpha,
-                            betad, energy, xAxis)
+                            beta, energy, xAxis)
         iters += 1
         if iters > 5000
-            println("alpha = $alpha , betad = $betad DNF")
+            println("alpha = $alpha , beta = $beta DNF")
             return dt*iters
         end
     end
     ret = dt*iters
 end
+#
+# hopping_time(potentialTup, bump, P0, temperature, alpha, beta, heat_capacity,
+#                     xAxis; dt=1e-4, tol=0.7)
+nPoints = 250
+alphaMin = -0.0001
+alphaMax = -0.0005
+betaMin = 0.00001
+betaMax = 0.005
+betaVec = linspace(betaMin, betaMax, nPoints)
+alphaVec = linspace(alphaMin, alphaMax, nPoints)
+@time a = Float64[hopping_time(potentialTup, bump, P0, temperature, alpha, beta,
+                               xAxis)
+                    for alpha in linspace(alphaMin, alphaMax, nPoints),
+                    beta in linspace(betaMin, betaMax, nPoints)]
+# @time a = map((alpha, beta) -> hopping_time(potentialTup, bump, P0, temperature, alpha, beta,
+#                     heat_capacity, xAxis),
+#                     linspace(0.0, 0.8, nPoints),
+#                     linspace(1, 4, nPoints))
 
-hopping_time(alpha, betad) = hopping_time(potentialTup, bump, P0,
-                temperature, alpha, betad, xAxis)
-
-end # Module.
+matshow(a)
+xlabel(L"\beta")
+ylabel(L"\alpha")
+figure()
+surf(betaVec, alphaVec, a)
+xlabel(L"\beta")
+ylabel(L"\alpha")
